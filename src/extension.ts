994618ce51f78479a419db878cb874cdb02ab697
Sergii Solomka <sergii.solomka+github@gmail.com>
2022-05-09 23:35:31 +0300

    config variables;

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import pss from './powershellprocessor';
- 
+
 var path = require("path");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,15 +11,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "builder365solution" is now active!');
-	
-	var currentlyOpenTabfileName = path.basename('');
+
+	const configuration = vscode.workspace.getConfiguration();
+	// 3) Get the current value
+	const solutionUniqueName = configuration.get('Build.SolutionName');
+	const schemaNameFile = configuration.get('Build.ShemaNameFileJs');
+	const productionJSPath = configuration.get('Build.ProductionJSPath');
+	vscode.window.showInformationMessage('Hello World from Build SolutionCRM!');
+
+	let runBuildSolution = 'C:\\CRMDeveloperTools\\VsCodeExtension\\CreateSolutionWIthResources.ps1 '
+		+ '-SolutionUniqueName "' + solutionUniqueName + '"'
+		+ '-SchemaNameFile "' + schemaNameFile + '"'
+		+ '-PathToSolutionPack "C:\\CRMDeveloperTools\\TmpSolution\\NewSolution"'
+		+ '-ProductionJSPath "' + productionJSPath + '"';
+	let runPackSolution = 'C:\\CRMDeveloperTools\\Tools\\CoreTools\\solutionpackager.exe '
+		+ '/action:Pack '
+		+ '/zipfile:"C:\\CRMDeveloperTools\\TmpSolution\\NewSolution.zip" '
+		+ '/folder:"C:\\CRMDeveloperTools\\TmpSolution\\NewSolution"';
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('builder365solution.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		console.log(pss.process(['']));
+		var t = pss.process([runBuildSolution, runPackSolution]);
+		console.log('test ' + t);
 		vscode.window.showInformationMessage('Hello World from builder365solution!');
 	});
 
--- src/powershellprocessor.ts
+++ src/powershellprocessor.ts
@@ -1,28 +1,31 @@
 const PSRunner = {
 
     process: function (commands: string[]) {
-        let self: {
-            out: string[];
-            err: string[];
-        };
-        const results: { command: string; output: any; errors: any }[] = [];
+        // let self: {
+        //     out: [];
+        //     err: [];
+        // }=[];
+
+        const results: { command: string; /*output: any; errors: any*/ }[] = [];
         var spawn = require("child_process").spawn;
         var child = spawn("powershell.exe", ['-Command', '-']);
         child.stdout.on("data", function (data: any) {
-            self.out.push(data.toString());
+            // self.out.push(data.toString());
+            console.log(data.toString());
         });
         child.stderr.on("data", function (data: any) {
-            self.err.push(data.toString());
+            // self.err.push(data.toString());
+            console.log(data.toString());
         });
         child.on("exit", function () {
             console.log("Powershell Script finished");
         });
 
         commands.forEach(function (cmd) {
-            self.out = [];
-            self.err = [];
+            // self.out = [];
+            // self.err = [];
             child.stdin.write(cmd + '\n');
-            results.push({ command: cmd, output: self.out, errors: self.err });
+            results.push({ command: cmd/*, output: self.out, errors: self.err*/ });
         });
 
         child.stdin.end(); //end input
